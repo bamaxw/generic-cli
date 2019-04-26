4a5115de6c8ff07ec3166ca2a19d307ccd690482
maximus <max.wasylow@trinitymirror.com>
2019-04-26 10:40:46 +0100

    same methods of different instances will return different values

--- example.py
+++ example.py
@@ -6,6 +6,8 @@ from generic_cli import AutoResolveClient
 async def main():
     async with AutoResolveClient('skyscraper-api', 'stag') as cli:
         print(cli, cli.__dict__, await cli.get_host())
+    async with AutoResolveClient('nonexistant', 'stag') as cli:
+        print(cli, cli.__dict__, await cli.get_host())
 
 
 if __name__ == '__main__':
--- generic_cli/utils.py
+++ generic_cli/utils.py
@@ -1,5 +1,5 @@
 '''Utility functions and classes for core generic-cli'''
-from typing import Any, Awaitable, Callable, Dict, TypeVar, Union
+from typing import Any, Awaitable, Callable, cast, Dict, TypeVar, Union
 from functools import wraps
 import inspect
 import time
@@ -33,15 +33,31 @@ class CacheFor:
     def __call__(self, func: AsyncCallable) -> AsyncCallable:
         sig = inspect.signature(func)
         params = sig.parameters
-        if len(params) > 1 or (len(params) == 1 and 'self' not in params):
-            raise TypeError('CacheFor can be called on a method or function with no arguments only!'
-                            f' Detected signature: {str(sig)}')
+        if len(params) == 1 and 'self' in params:
+            return self._decorate_func(cast(AsyncFunc, func))
+        if not params:
+            return self._decorate_method(cast(AsyncMethod, func))
+        raise TypeError('CacheFor can be called on a method or function with no arguments only!'
+                        f' Detected signature: {str(sig)}')
+
+    def _decorate_func(self, func: AsyncFunc[T]) -> AsyncFunc[T]:
         self._func_timestamps[func] = 0
         @wraps(func)
-        async def _method_wrapper(*a, no_cache: bool = False):
+        async def _func_wrapper(no_cache: bool = False) -> T:
             next_call_ts = self._func_timestamps[func]
             if no_cache or next_call_ts <= time.time():
-                self._func_cache[func] = await func(*a)
+                self._func_cache[func] = await func()
                 self._func_timestamps[func] = time.time() + self.timeout
             return self._func_cache[func]
+        return _func_wrapper
+
+    def _decorate_method(self, func: AsyncMethod[T]) -> AsyncMethod[T]:
+        @wraps(func)
+        async def _method_wrapper(self, no_cache: bool = False) -> T:
+            key = (self, func)
+            next_call_ts = self._func_timestamps.get(key)
+            if no_cache or next_call_ts <= time.time():
+                self._func_cache[key] = await func(self)
+                self._func_timestamps[key] = time.time() + self.timeout
+            return self._func_cache[key]
         return _method_wrapper
