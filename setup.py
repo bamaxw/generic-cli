ef31ab8d0bbc82e7bf7861568cbadb1744211089
maximus <max.wasylow@trinitymirror.com>
2019-04-26 14:23:15 +0100

    added tenacity to deps

--- setup.py
+++ setup.py
@@ -19,4 +19,4 @@ setup(name="generic-cli",
       long_description=read("README.md"),
       packages=find_packages(),
       python_requires=">=3.7",
-      install_requires=['aiohttp', 'crossroads'])
+      install_requires=['aiohttp', 'crossroads', 'tenacity'])
