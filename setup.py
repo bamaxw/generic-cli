eae7f955afde15245998eea8f4a91ec04bfb176e
maximus <max.wasylow@trinitymirror.com>
2019-04-26 14:11:16 +0100

    Bump version: 0.0.7 → 0.0.8

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ def read(file_name):
 
 setup(name="generic-cli",
       author="Maximus Wasylow",
-      version='0.0.7',
+      version='0.0.8',
       author_email="bamwasylow@gmail.com",
       description="Python API for remote jigsaw connections",
       long_description=read("README.md"),
